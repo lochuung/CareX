c47241bbc5b3477d83e1cb8146f893d6daa36c45
Nguyen Huu Loc <locn562836@gmail.com>
2024-05-18 13:07:53 +0700

    server setup

---
+++ server/src/main/java/org/webdev/carex/CareXApplication.java
@@ -0,0 +1,13 @@
+package org.webdev.carex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareXApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/org/webdev/carex/CareXApplicationTests.java
@@ -0,0 +1,13 @@
+package org.webdev.carex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
