6dee751fc14ba9aefc71675222221c40c50cc9a4
Nguyen Huu Loc <locn562836@gmail.com>
2024-05-18 16:05:55 +0700

    common response dto setup

---
+++ server/src/main/java/org/webdev/carex/constant/ResponseConstant.java
@@ -0,0 +1,9 @@
+package org.webdev.carex.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ResponseConstant {
+    public static final String SUCCESS = "success";
+    public static final String FAIL = "fail";
+}
