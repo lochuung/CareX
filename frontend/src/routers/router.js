eb47fbd8534d6ac25b1ae2932eeba6314bf4cb38
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2024-05-24 23:56:52 +0700

    Update: Add ML Yoga

--- frontend/src/routers/router.js
+++ frontend/src/routers/router.js
@@ -3,7 +3,7 @@ import { HomePage, Login, Signup, Workshop, Yoga } from "../pages";
 export const router = [
   { path: "/login", element: Login },
   { path: "/signup", element: Signup },
-  { path: "/", element: HomePage },
+  { path: "/home", element: HomePage },
   { path: "/workshop", element: Workshop },
   { path: "/yoga", element: Yoga },
 ];
