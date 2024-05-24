33551d34e9da7cb61056f52cfa4fd50df7d91f86
dgeniust <dathiichan141@gmail.com>
2024-05-24 22:54:33 +0700

    profile

--- frontend/src/routers/router.js
+++ frontend/src/routers/router.js
@@ -1,4 +1,4 @@
-import { HomePage, Login, Signup, Workshop, Yoga } from "../pages";
+import { HomePage, Login, Signup, Workshop, Yoga, EditProfile, ChoosePlan, Notifications, PasswordAndSecurity } from "../pages";
 
 export const router = [
   { path: "/login", element: Login },
@@ -6,4 +6,8 @@ export const router = [
   { path: "/", element: HomePage },
   { path: "/workshop", element: Workshop },
   { path: "/yoga", element: Yoga },
+  { path: "/edit-profile", element: EditProfile },
+  { path: "/choose-plan", element: ChoosePlan },
+  { path: "/notifications", element: Notifications },
+  { path: "/password-security", element: PasswordAndSecurity },
 ];
