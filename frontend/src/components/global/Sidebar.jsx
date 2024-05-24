79b03179d3f171ea8ee087c1cc5362da9bbb5a3a
Tam-2002-SGU <tam.le@heysheep.sg>
2024-05-24 17:10:30 +0700

    add sidebar

--- frontend/src/components/global/Sidebar.jsx
+++ frontend/src/components/global/Sidebar.jsx
@@ -1,29 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  AppstoreOutlined,
+  CalendarOutlined,
+  LinkOutlined,
+  MailOutlined,
+  SettingOutlined,
+  HomeOutlined,
+  WechatWorkOutlined,
+  UserOutlined,
+  BookOutlined,
+} from "@ant-design/icons";
+import { Avatar, Dropdown, Layout, Menu, theme } from "antd";
 
-const Sidebar = () => {
+const { Header, Content, Footer, Sider } = Layout;
+
+const Sidebar = ({ collapsed, onCollapsed }) => {
   const navigate = useNavigate();
-  const link = [
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  const userItems = [
     {
-      name: "Home",
-      path: "/",
+      key: "/home",
+      icon: <HomeOutlined />,
+      label: "Trang chủ",
     },
     {
-      name: "workshop",
-      path: "/workshop",
+      key: "/workshops",
+      icon: <WechatWorkOutlined />,
+      label: "Workshop",
     },
+
     {
-      name: "yoga",
-      path: "/yoga",
+      key: "/yoga",
+      icon: <UserOutlined />,
+      label: "Yoga",
+    },
+
+    {
+      key: "/food",
+      icon: <BookOutlined />,
+      label: "Gợi ý bữa ăn",
+    },
+
+    {
+      key: "/setting",
+      icon: <BookOutlined />,
+      label: "Cài đặt",
     },
   ];
+
+  function handleClick(e) {
+    console.log("click", e);
+    console.log(e.key, "key");
+    navigate(e.key);
+  }
   return (
-    <div className="flex flex-col gap-3">
-      {link.map((item) => (
-        <div onClick={() => navigate(item?.path)} className="bg-yellow-300 p-2">
-          <h1>{item?.name}</h1>
+    <div className="flex flex-col gap-3 h-full bg-red-500">
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={() => onCollapsed()}
+        style={{
+          overflow: "auto",
+          height: "100vh",
+          position: "fixed",
+          left: 0,
+          top: 0,
+          bottom: 0,
+        }}
+      >
+        <div className="demo-logo-vertical">
+          <h1
+            className={` text-blue-600 font-extrabold text-3xl text-center my-3 items-center justify-center ${
+              collapsed ? "hidden" : "flex"
+            }`}
+          >
+            CareX
+          </h1>
         </div>
-      ))}
+        <Menu
+          theme="dark"
+          onClick={handleClick}
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={userItems}
+        />
+      </Sider>
     </div>
   );
 };
--- frontend/src/layouts/DefaultLayout.jsx
+++ frontend/src/layouts/DefaultLayout.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/global/Sidebar";
+import { Avatar, Dropdown, Layout, Menu, theme } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
 
 const DefaultLayout = ({ children }) => {
-  return (
-    <div className="flex ">
-      <div className="">
-        <Sidebar />
-      </div>
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
-      <div className="">{children}</div>
-    </div>
+  const [collapsed, setCollapsed] = useState(false);
+  const handleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+  return (
+    <Layout hasSider>
+      <Sidebar collapsed={collapsed} onCollapsed={handleCollapsed} />
+      <Layout
+        style={{
+          marginLeft: collapsed ? 60 : 200,
+          flex: 1,
+        }}
+      >
+        <Content
+          style={{
+            margin: "24px 16px 0",
+            overflow: "initial",
+          }}
+        >
+          <div
+            style={{
+              padding: 24,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            {children}
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
