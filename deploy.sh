177e9634680516d8f52616a9ffe05702078e2069
Nguyen Huu Loc <locn562836@gmail.com>
2024-05-25 13:42:16 +0700

    optimize the deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,48 +1,31 @@
 #!/bin/bash
+# Usage: ./deploy.sh
 
-# Usage: ./deploy.sh --sql_user mysql_user --sql_password mysql_password --api_url api_url
-
-# Set the environment variables
-while [[ $# -gt 0 ]]
-do
-key="$1"
-
-case $key in
-    --sql_user)
-    SQL_USER="$2"
-    shift
-    shift
-    ;;
-    --sql_password)
-    SQL_PASSWORD="$2"
-    shift
-    shift
-    ;;
-    --api_url)
-    API_URL="$2"
-    shift
-    shift
-    ;;
-    *)
-    shift
-    ;;
-esac
-done
+# Load the environment variables from the .env file
+if [ -f .env ]; then
+    export $(cat .env | sed 's/#.*//g' | xargs)
+fi
 
 # Check if the environment variables are set
-if [ -z "$SQL_USER" ] || [ -z "$SQL_PASSWORD" ] || [ -z "$API_URL" ]; then
-    echo "Usage: ./deploy.sh --sql_user mysql_user --sql_password mysql_password --api_url api_url"
+if [ -z "$SQL_USER" ] || [ -z "$SQL_PASSWORD" ] || [ -z "$API_URL" ] || [ -z "$MAIL_USERNAME" ] || [ -z "$MAIL_PASSWORD" ] || [ -z "$ADMIN_EMAIL" ] || [ -z "$ADMIN_PASSWORD" ] || [ -z "$JWT_SECRET_KEY" ]; then
+    echo "Please set the environment variables in the .env file"
     exit 1
 fi
 
-# Set the environment variables in the .env file
-echo "SQL_USER=$MYSQL_USER" > .env
-echo "SQL_PASSWORD=$MYSQL_PASSWORD" >> .env
-echo "API_URL=$API_URL" >> .env
+# Create volumes if they do not exist
+if [ "$(docker volume ls -q -f name=carex-mysql)" != "carex-mysql" ]; then
+    docker volume create carex-mysql
+fi
+if [ "$(docker volume ls -q -f name=maven-repo)" != "maven-repo" ]; then
+    docker volume create maven-repo
+fi
 
-# Create volumes if it does not exist
-docker volume create carex-mysql
-docker volume create maven-repo
+# Validate the Docker Compose file
+docker-compose -f docker-compose-prd.yml config
+if [ $? -ne 0 ]; then
+    echo "Invalid Docker Compose file"
+    exit 1
+fi
 
 # Deploy the Docker Compose file
 docker-compose -f docker-compose-prd.yml up --build -d
